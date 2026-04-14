da2a1fa311001bdc4a555488d1bf7cce1c284819
izzy <mendozaisaac101@gmail.com>
2026-04-14 15:18:51 -0700

    feat: add circle-based collision detection and game over state

--- circleshape.py
+++ circleshape.py
@@ -16,7 +16,11 @@ class CircleShape(pygame.sprite.Sprite):
     def draw(self, screen):
         # must override
         pass
-
+    
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        distance = self.position.distance_to(other.position)
+        return distance <= (self.radius + other.radius)
--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 import pygame 
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from circleshape import CircleShape
 
 def main():
     #Initialize pygame
     pygame.init()
     print("Starting Asteroids with pygame version: 2.6.1")
+    #Create new clock object
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    AsteroidField()
+
+    # draw player in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
         for event in pygame.event.get():
+            #check if the user has closed the window, and exit the game loop if they do.
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
-        pygame.display.flip()
-    
+        updatable.update(dt)
 
+        #Check if any asteorids collide with player
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
